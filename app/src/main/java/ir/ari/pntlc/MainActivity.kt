5ada677e2d7b09c623ce4dcd6ed76941df52e6d8
Alireza Ivaz <ivaz1382@gmail.com>
2023-05-18 20:58:15 +0330

    Migrate from Kotlin Android Extensions to ViewBinding

--- app/src/main/java/ir/ari/pntlc/MainActivity.kt
+++ app/src/main/java/ir/ari/pntlc/MainActivity.kt
@@ -5,19 +5,21 @@ import android.text.Editable
 import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import ir.ari.nlc.PersianNumbersToLettersConverter
-import kotlinx.android.synthetic.main.activity_main.*
+import ir.ari.pntlc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        mainLayout.setOnClickListener(null)
-        numberInput.addTextChangedListener(object : TextWatcher {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mainLayout.setOnClickListener(null)
+        binding.numberInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
             override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
                 val nlc = PersianNumbersToLettersConverter()
-                val result = nlc.getParsedString(numberInput.text.toString())
-                numberOutput.text = result
+                val result = nlc.getParsedString(binding.numberInput.text.toString())
+                binding.numberOutput.text = result
             }
             override fun afterTextChanged(editable: Editable) {}
         })
