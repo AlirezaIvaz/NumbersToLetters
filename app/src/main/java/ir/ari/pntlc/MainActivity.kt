5cf2aaf905336c45ad068e71810b2350e9a3be78
Alireza Ivaz <ivaz1382@gmail.com>
2023-05-18 21:42:48 +0330

    Add toolbar to demo

--- app/src/main/java/ir/ari/pntlc/MainActivity.kt
+++ app/src/main/java/ir/ari/pntlc/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
         binding.mainLayout.setOnClickListener(null)
         binding.numberInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
