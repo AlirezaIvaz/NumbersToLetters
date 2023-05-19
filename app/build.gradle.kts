caedf809b7cd446d6b6c9d940f8e1d4650b122ce
Alireza Ivaz <ivaz1382@gmail.com>
2023-05-19 18:52:43 +0330

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "ir.alirezaivaz.numberstoletters"
         minSdk = 21
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
         resourceConfigurations += "fa"
     }
     buildTypes {
