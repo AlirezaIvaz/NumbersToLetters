7530632b8a0b75ec56fa121f3cdcc6d2c6727577
Alireza Ivaz <ivaz1382@gmail.com>
2023-06-27 18:09:29 +0330

    Improve FAB functionality

--- app/src/main/kotlin/ir/alirezaivaz/numberstoletters/demo/ActivityMain.kt
+++ app/src/main/kotlin/ir/alirezaivaz/numberstoletters/demo/ActivityMain.kt
@@ -12,6 +12,7 @@ import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
 import androidx.core.os.LocaleListCompat
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import ir.alirezaivaz.numberstoletters.PersianNumbersToLettersConverter
 import ir.alirezaivaz.numberstoletters.demo.databinding.ActivityMainBinding
 
@@ -24,6 +25,7 @@ class ActivityMain : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        binding.fab.initGitHubFab()
         val numbersToLettersConverter = PersianNumbersToLettersConverter()
         binding.numberInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
@@ -40,10 +42,13 @@ class ActivityMain : AppCompatActivity() {
 
             override fun afterTextChanged(editable: Editable) {}
         })
-        Handler(Looper.getMainLooper()).postDelayed({
-            binding.fab.shrink()
-        }, 2000)
-        binding.fab.setOnClickListener {
+    }
+
+    private fun ExtendedFloatingActionButton.initGitHubFab() {
+        smoothShrink()
+        this.setOnClickListener {
+            extend()
+            smoothShrink()
             val params = CustomTabColorSchemeParams.Builder()
                 .setToolbarColor(ContextCompat.getColor(activityMain, R.color.github))
                 .build()
@@ -56,5 +61,17 @@ class ActivityMain : AppCompatActivity() {
                     Uri.parse("https://github.com/AlirezaIvaz/NumbersToLetters")
                 )
         }
+        setOnLongClickListener {
+            extend()
+            smoothShrink()
+            true
+        }
     }
+
+    private fun ExtendedFloatingActionButton.smoothShrink() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            this.shrink()
+        }, 2000)
+    }
+
 }
